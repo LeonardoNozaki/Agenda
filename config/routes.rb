200f1e1ca806e25553c07e1e29e31a78a749b08e
lucaspsacchi <lucaspsacchi12345@hotmail.com>
2019-06-26 13:39:46 -0300

    VersInício do front-end

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'eventos/new'
   get 'eventos/create'
   get 'eventos/delete'
-  get '/home', to: 'pages#home'
   get '/info', to: 'pages#info'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
