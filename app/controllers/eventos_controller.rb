44fb46f4a7cef39ee788b04ee66d1baaced68cec
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-06-27 23:04:45 -0300

    controller eventos faz a busca em um dia especifico para o usuario logado

--- app/controllers/eventos_controller.rb
+++ app/controllers/eventos_controller.rb
@@ -4,11 +4,12 @@ class EventosController < ApplicationController
       redirect_to new_user_session_path
     end
 
-    @var = Evento.all
+    #@var = Evento.all
+	@var = Evento.where( "user_id = ? AND data >= ? AND data < ? ", current_user.id, Date.parse("20190626"), Date.parse("20190627"))
     @var1 = User.all
 
-    @evs = Event.where(user_id: current_user.id)
-    @evs = curren_user.events
+    #@evs = Event.where(user_id: current_user.id)
+    #@evs = curren_user.events
   end
 
   def new
