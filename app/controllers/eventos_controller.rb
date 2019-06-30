e2caeab50c198080ea0c8d4950f9b0f648f6af9f
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-06-30 00:18:49 -0300

    altas mudancas, ta funcionando o create, index, find e delete de eventos

--- app/controllers/eventos_controller.rb
+++ app/controllers/eventos_controller.rb
@@ -4,8 +4,13 @@ class EventosController < ApplicationController
       redirect_to new_user_session_path
     end
 
-    #@var = Evento.all
-	@var = Evento.where( "user_id = ? AND data >= ? AND data < ? ", current_user.id, Date.parse("20190626"), Date.parse("20190627"))
+    @d = Date.today
+    @t = Time.find_zone("UTC").parse("00:00")
+    @t1 = Time.find_zone("UTC").parse("23:59")
+    @dataInicial = DateTime.new(@d.year, @d.month, @d.day, @t.hour, @t.min, @t.sec, @t.zone)
+    @dataFinal = DateTime.new(@d.year, @d.month, @d.day, @t1.hour, @t1.min, @t1.sec, @t1.zone)
+
+    @var = Evento.where("user_id = ? AND data >= ? AND data <= ? ", current_user.id, @dataInicial, @dataFinal)
     @var1 = User.all
 
     #@evs = Event.where(user_id: current_user.id)
@@ -15,9 +20,45 @@ class EventosController < ApplicationController
   def new
   end
 
+  def find
+    if !user_signed_in?
+      redirect_to new_user_session_path
+    end
+
+    @d = Date.parse("" + params[:data])
+    @t = Time.find_zone("UTC").parse("00:00")
+    @t1 = Time.find_zone("UTC").parse("23:59")
+    @dataInicial = DateTime.new(@d.year, @d.month, @d.day, @t.hour, @t.min, @t.sec, @t.zone)
+    @dataFinal = DateTime.new(@d.year, @d.month, @d.day, @t1.hour, @t1.min, @t1.sec, @t1.zone)
+
+    @var = Evento.where("user_id = ? AND data >= ? AND data <= ? ", current_user.id, @dataInicial, @dataFinal)
+    @var1 = User.all
+  end
+
+  def create_evento
+    @var = Evento.new
+    @var.user_id = current_user.id
+    @var.nome = params[:nome]
+    @var.data = params[:data]
+    @var.desc = params[:desc]
+    if @var.save
+      redirect_to eventos_index_path
+    else
+      render :new
+    end
+  end
+
   def create
   end
 
   def delete
+    @evento = Evento.find(params[:id])
+
+    if @evento.destroy
+      redirect_to eventos_index_path
+    else
+      render :show
+    end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   get 'eventos/index'
   get 'eventos/new'
-  get 'eventos/create'
+  #get 'eventos/create'
+  #post 'eventos/create'
+  post 'eventos/create_evento'
+  post 'eventos/find'
   get 'eventos/delete'
-  get '/info', to: 'pages#info'
+  #get '/info', to: 'pages#info'
   root to: 'pages#home'
+
+  #resources :eventos
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
